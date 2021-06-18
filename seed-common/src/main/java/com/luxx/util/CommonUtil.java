5498462ed55d9bb7fde1f18b16b5923fc8c3a3a9
luxiaoxun <luxiaoxun001@qq.com>
2021-06-18 16:19:46 +0800

    Update common util

--- seed-common/src/main/java/com/luxx/util/CommonUtil.java
+++ seed-common/src/main/java/com/luxx/util/CommonUtil.java
@@ -63,4 +63,28 @@ public class CommonUtil {
         return list.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
 
+    private static final String BASE = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static String toBase62(long num) {
+        StringBuilder sb = new StringBuilder();
+        int targetBase = BASE.length();
+        do {
+            int i = (int) (num % targetBase);
+            sb.append(BASE.charAt(i));
+            num /= targetBase;
+        } while (num > 0);
+        return sb.reverse().toString();
+    }
+
+    public static long toBase10(String input) {
+        int srcBase = BASE.length();
+        long id = 0;
+        String r = new StringBuilder(input).reverse().toString();
+        for (int i = 0; i < r.length(); i++) {
+            int charIndex = BASE.indexOf(r.charAt(i));
+            id += charIndex * (long) Math.pow(srcBase, i);
+        }
+        return id;
+    }
+
 }
